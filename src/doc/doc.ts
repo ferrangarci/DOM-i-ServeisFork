5254beb4c7f657d4e17c97d4fc98fe878f87ed35
kituss44 <marc.romaguera@institutvidreres.cat>
2025-12-11 08:22:20 +0100

    starting

---
+++ src/doc/doc.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doc',
+  imports: [],
+  templateUrl: './doc.html',
+  styleUrl: './doc.css',
+})
+export class Doc {
+
+}
